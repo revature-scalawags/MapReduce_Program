6d06299211eb092e6953c3fb353aa89fc218995b
spark131008 <spark131008@gmail.com>
2020-12-31 23:52:52 -0500

    edited and updated

--- src/main/scala/mapreducer/EnglishPageViewCount.scala
+++ src/main/scala/mapreducer/EnglishPageViewCount.scala
@@ -2,6 +2,7 @@ package mapreducer
 
 import java.io.{BufferedReader, FileReader, IOException}
 import java.lang.Iterable
+import java.util.StringTokenizer
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
@@ -15,43 +16,45 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
 
+
 object EnglishPageViewCount {
   
   class PageViewMap extends Mapper[LongWritable, Text, Text, IntWritable]{
 
+    // val one = new IntWritable(1)
+
     @throws[IOException]
     override def map(key: LongWritable, value: Text, context: Mapper[LongWritable, Text, Text, IntWritable]#Context): Unit ={
       val line: String = value.toString
-      
+
       //Showing only English Wikipedia pages
-      if(line.split("\\s")(0).contains("en")){
+      if(line.substring(0,2).equals("en")){
         val pageName: String = line.split("\\s")(1)
-        //var pageViews = new IntWritable(line.split("\\s")(2).toInt)
-        line.split("\\s").filter(_.length>0).foreach((word: String) => {
-          context.write(new Text(pageName), new IntWritable(1))
-        })
-      }else{}
+        var pageViews = line.split("\\s")(2).toInt
+        context.write(new Text(pageName), new IntWritable(pageViews))
+      }
     }
   }
 
   class PageViewReduce extends Reducer[Text, IntWritable, Text, IntWritable]{
     @throws[IOException]
     override def reduce(key: Text, values: Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
-      var sum = values.asScala.foldLeft(0)(_+_.get())
+      var sum = 0
+      values.forEach(sum += _.get())
       context.write(key, new IntWritable(sum))
     }
-
   }
 
 
   def main(args: Array[String]): Unit = {
-    val configuration = new Configuration
-    val job = Job.getInstance(configuration, "EnglishPageViewCount")
+    // val configuration = new Configuration
+    val job = Job.getInstance()
 
     job.setJarByClass(this.getClass)
+    job.setJobName("EnglishPageViewCount")
     job.setMapperClass(classOf[PageViewMap])
 
-    job.setCombinerClass(classOf[PageViewReduce])
+    // job.setCombinerClass(classOf[PageViewReduce])
     job.setReducerClass(classOf[PageViewReduce])
 
     job.setInputFormatClass(classOf[TextInputFormat])
@@ -60,9 +63,8 @@ object EnglishPageViewCount {
 
     FileInputFormat.setInputPaths(job, new Path(args(0)))
     FileOutputFormat.setOutputPath(job, new Path(args(1)))
-    System.exit(if(job.waitForCompletion(true)) 0 else 1 )
+    val success = job.waitForCompletion(true)
+    System.exit(if(success) 0 else 1 )
 
   }
-
-
 }
